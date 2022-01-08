f1b765c62b2d39b5e67bfd32ccad068eaa350059
MahoMori <tr27e1104@gmail.com>
2022-01-08 13:03:36 -0800

    added code to retrieve image with api

--- src/App.js
+++ src/App.js
@@ -1,5 +1,32 @@
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+
+const url = `https://api.nasa.gov/planetary/apod?api_key=${process.env.REACT_APP_API_KEY}&count=20`;
+
 function App() {
-  return <div></div>;
+  const [images, setImages] = useState([]);
+
+  const fetchImages = async () => {
+    const { data } = await axios.get(url);
+    setImages(data);
+  };
+
+  useEffect(() => {
+    fetchImages();
+  }, []);
+
+  return (
+    <div>
+      <ul>
+        {images.length > 0 &&
+          images.map((image) => (
+            <li key={image.date}>
+              <img src={image.url} alt={image.title} />
+            </li>
+          ))}
+      </ul>
+    </div>
+  );
 }
 
 export default App;
