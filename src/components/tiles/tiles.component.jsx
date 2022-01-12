83d7974a6e77dce0e6a5c1df0c8a532a3d076bcc
MahoMori <tr27e1104@gmail.com>
2022-01-11 19:15:50 -0800

    added styled component

--- src/components/tiles/tiles.component.jsx
+++ src/components/tiles/tiles.component.jsx
@@ -1,21 +1,39 @@
 import React from "react";
 
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faHeart as fasHeart } from "@fortawesome/free-solid-svg-icons";
+import { faHeart as farHeart } from "@fortawesome/free-regular-svg-icons";
+
+import {
+  TilesContainer,
+  EachTile,
+  InfoContainer,
+  TitleDate,
+  Description,
+  TileImage,
+} from "./tiles.style.js";
+
 function Tiles({ images, toFav }) {
   return (
-    <div>
+    <TilesContainer>
       {images.length > 0 &&
         images.map((image) => (
-          <div key={image.date}>
-            <img
+          <EachTile key={image.date}>
+            <FontAwesomeIcon icon={fasHeart} style={{ color: "#FF84FF" }} />
+            <InfoContainer>
+              <TitleDate></TitleDate>
+              <Description></Description>
+            </InfoContainer>
+            <TileImage
               src={image.url}
               alt={image.title}
               onClick={() => {
                 toFav(image);
               }}
             />
-          </div>
+          </EachTile>
         ))}
-    </div>
+    </TilesContainer>
   );
 }
 
--- src/components/tiles/tiles.style.js
+++ src/components/tiles/tiles.style.js
@@ -1,3 +1,8 @@
 import styled from "styled-components";
 
+export const TilesContainer = styled.div``;
 export const EachTile = styled.div``;
+export const InfoContainer = styled.div``;
+export const TitleDate = styled.p``;
+export const Description = styled.p``;
+export const TileImage = styled.img``;
